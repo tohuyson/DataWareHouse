6766bbbcaa7f9f73b3a67e474a8235237f1557d5
Nguyen Thi Giang <17130047@st.hcmuaf.edu.vn>
2020-07-26 19:45:26 +0700

    giang - thêm điều kiện chạy theo config

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import load_local_to_staging.LoadFromLocalToStaging;
+import load_staging_to_warehouse.StagingtoWarehouse;
+
+public class Main extends Thread{
+	DownloadScp dowload;
+	LoadFromLocalToStaging loadStaging;
+	StagingtoWarehouse loadWH;
+	public Main() {
+		
+	}
+}
