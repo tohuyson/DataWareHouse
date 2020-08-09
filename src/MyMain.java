28e0dfc214c2001b0ef385cd097e482022302534
Nguyen Thi Giang <17130047@st.hcmuaf.edu.vn>
2020-08-09 17:16:48 +0700

    giang - send mail

--- src/MyMain.java
+++ src/MyMain.java
@@ -5,18 +5,19 @@ import java.util.TimerTask;
 import load_staging_to_warehouse.StagingtoWarehouse;
 
 public class MyMain {
-//	static DownloadScp dowload = new DownloadScp();
-
-//	public static void main(String[] args) {
-//		try {
-//			System.out.println(args[0]);
-//			dowload.downloadFile(Integer.parseInt(args[0]));
-////			loadStaging.staging("my_logs.status_download = 'OK Download' AND my_logs.id_config=" + args[0]);
-//			loadWH.LoadStagingtoWarehouse(Integer.parseInt(args[0]));
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	// static DownloadScp dowload = new DownloadScp();
+	// static LoadFromLocalToStaging loadStaging= new LoadFromLocalToStaging();
+	//
+	// public static void main(String[] args) {
+	// try {
+	// System.out.println(args[0]);
+	//// dowload.downloadFile(Integer.parseInt(args[0]));
+	// loadStaging.staging("OK Download", Integer.parseInt(args[0]),7);
+	//// loadWH.LoadStagingtoWarehouse(Integer.parseInt(args[0]));
+	// } catch (Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
 
 	public static void main(String[] args) {
 		StagingtoWarehouse loadWH = new StagingtoWarehouse();
@@ -24,6 +25,7 @@ public class MyMain {
 		TimerTask task = new TimerTask() {
 
 			int count = 1;
+
 			@Override
 			public void run() {
 				try {
